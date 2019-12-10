a54a3c701e6d1ec9a26004a2d5403253c8f233a1
Miao ZhiCheng <miao@decentral.ee>
2019-12-10 10:25:22 +0200

    fix workflows

--- truffle-config.js
+++ truffle-config.js
@@ -46,12 +46,6 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
 
-        development: {
-            host: "127.0.0.1",     // Localhost (default: none)
-            port: 8545,            // Standard Ethereum port (default: none)
-            network_id: "*",       // Any network (default: none)
-        },
-
         rinkeby: {
             provider: () => new HDWalletProvider(
                 process.env.RINKEBY_MNEMONIC,
