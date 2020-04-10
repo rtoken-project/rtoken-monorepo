d98251490e600935e1d865a63e327b2f19f2dc85
Miao ZhiCheng <miao@decentral.ee>
2020-04-10 18:59:43 +0300

    rename to get-token-info

--- scripts/get-token-info.js
+++ scripts/get-token-info.js
@@ -3,9 +3,16 @@ const PROXIABLE_UUID = "0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d587
 module.exports = async function (callback) {
     try {
         global.web3 = web3;
+
+        const IRToken = artifacts.require("IRToken");
+
         const proxyAddress = process.argv[process.argv.length - 1];
         const logicAddress = (await web3.eth.getStorageAt(proxyAddress, PROXIABLE_UUID)).substr(-40, 40);
         console.log(`PROXIABLE contract ${proxyAddress} has its logic address at 0x${logicAddress}`);
+
+        const rToken = await IRToken.at(proxyAddress);
+        console.log("Allocation Strategy: ", await rToken.ias.call());
+
         callback();
     } catch (err) {
         callback(err);
