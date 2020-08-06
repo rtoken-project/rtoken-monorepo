fff7b35fd3f818f850d62e010cc2b12ae5016137
Patrick <blockchainbuddha@gmail.com>
2020-08-06 13:32:48 -0400

    update test script

--- packages/utils/src/utils/constants.js
+++ packages/utils/src/utils/constants.js
@@ -3,6 +3,7 @@ const DEFAULT_NETWORK = "homestead";
 const SUBGRAPH_URLS = {
   homestead: "https://api.thegraph.com/subgraphs/name/rtoken-project/rdai",
   kovan: "https://api.thegraph.com/subgraphs/name/rtoken-project/rdai-kovan",
+  local: "http://localhost:8000/subgraphs/name/rtoken-test",
 };
 
 export { DEFAULT_NETWORK, SUBGRAPH_URLS };
--- packages/utils/test/utils/general.js
+++ packages/utils/test/utils/general.js
@@ -1,19 +1,20 @@
-import { InfuraProvider, JsonRpcProvider } from "@ethersproject/providers";
+import { JsonRpcProvider } from "@ethersproject/providers";
 import RTokenUtils, { getClient } from "../../src";
 
 export const getWeb3Provider = () => {
-  if (process.env.LOCAL) return new JsonRpcProvider("http://localhost:8545");
-  return new InfuraProvider("homestead", process.env.INFURA_KEY);
+  return new JsonRpcProvider("http://localhost:8545");
 };
 
 export const getRutils = () => {
-  const apolloInstance = getClient({
-    uri: process.env.SUBGRAPH_URL,
-    debug: false,
-  });
-
-  return new RTokenUtils(apolloInstance, getWeb3Provider(), {
-    debug: true,
-    network: process.env.LOCAL ? "local" : "homestead",
-  });
+  return new RTokenUtils(
+    getClient({
+      network: "local",
+      debug: true,
+    }),
+    getWeb3Provider(),
+    {
+      network: "local",
+      debug: true,
+    }
+  );
 };
